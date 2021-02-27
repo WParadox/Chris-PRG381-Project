10c0e596a974760b26f731de439ea8775fbf96aa
Kyle Opperman <Opperman45575@gmail.com>
2021-02-27 17:18:50 +0200

    Added classes (BusineesLayer(Customer_Bookings; CustomerData; EventSellablesItems; Owner) DataLayer (Changed DataHandler -> DataHandlerInputs + added methods); DataHandler_Output(first 3 methods); DataHdandler_Processess))<=====> Added comments

---
+++ src/main/java/DataAccessLayer/DataHandler_Output.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataAccessLayer;
+
+import BusinessLayer.*;
+import java.util.List;
+/**
+ *
+ * @author Christopher
+ */
+public class DataHandler_Output {
+    
+    public List<Client> ClientData()
+    {
+        return null;
+    }
+    
+    public List<Customer_Bookings> BookingData()
+    {
+        return null;
+    }
+    
+    public List<Event> EventData()
+    {
+        return null;
+    }
+    
+    public List<EventSellableItems> EventSellableItemsData()
+    {
+        return null;
+    }
+    
+    public List<Payment> PaymentData()
+    {
+        return null;
+    }
+    
+    public List<SellableItem> SellableItemsData()
+    {
+        return null;
+    }
+    
+    public List<Owner> Owner_Schedule()
+    {
+        return null;
+    }
+    
+}
