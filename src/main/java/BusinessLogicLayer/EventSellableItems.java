0221b5735ec08a009783776c259b42fdc6313850
KaramGoat <72029511+KaramGoat@users.noreply.github.com>
2021-02-27 21:36:08 +0200

    Added variables/fields and constructors to the following classes in the Business Logic Layer - Client - CustomerData - Event - EventSellableItems - Payment - SellableItem

---
+++ src/main/java/BusinessLogicLayer/EventSellableItems.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessLogicLayer;
+
+/**
+ *
+ * @author Kyle Opperman
+ */
+public class EventSellableItems 
+{
+    int event_id;
+    int eventitemid;
+    int quantity;
+    double discount;
+    
+    public EventSellableItems(int event_id, int eventitemid, int quantity, double discount)
+    {
+       this.event_id = event_id;
+       this.eventitemid = eventitemid;
+       this.quantity = quantity;
+       this.discount = discount;
+    }
+}
---
+++ src/main/java/BusinessLogicLayer/Payment.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessLogicLayer;
+
+/**
+ *
+ * @author Christopher
+ */
+public class Payment 
+{
+    int invoice_num;
+    double pay_date;
+    double amount;
+    int event_id;
+    
+    public Payment(int invoice_num, double pay_date, double amount, int event_id)
+    {
+        this.invoice_num = invoice_num;
+        this.pay_date = pay_date;
+        this.amount = amount;
+        this.event_id = event_id;
+        
+    }
+}
