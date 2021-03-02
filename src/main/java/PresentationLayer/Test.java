a2438f16475d5e475941f7fa2ce4c4e6a96494eb
Christopher-du-Plessis <cjm.dup@gmail.com>
2021-03-02 03:25:45 +0200

    Changed the physical separation of DatHandler input, process and output into a logical separation in the same class. Created the connection for the Database, Added query execution logic in the DataHandler Class, make extensive changes to the Client class including interaction with the DataHandler class, added a test class in the presentation layer.

---
+++ src/main/java/PresentationLayer/Test.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PresentationLayer;
+import BusinessLogicLayer.*;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.*;
+/**
+ *
+ * @author Christopher
+ */
+public class Test {
+    public static void main(String[] args) throws SQLException {
+        //Example of how the Presentation will interact with the business layer, In this example we display the firstname of each client in the Client table in the DB
+        Client clientObj = new Client();
+        List<Client> clients = clientObj.GetClients();
+        for (Client cl:clients) {
+            System.out.println(cl.getFirstName());
+        }
+        
+        //Example of inserting new client in database
+        Client clientToInsert = new Client("Nick","Morris",LocalDate.of(1990, Month.FEBRUARY, 24),"0735932363","nick.morris@gmail.com","9002244626963");
+        //clientObj.InsertNewClient(clientToInsert); //uncomment this code to perform row insertion
+        
+        //Example of updating a client in the database
+        
+        
+        
+    }
+}
